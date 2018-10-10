e15ae67913159d65ed9233c15c57523d7d01396f
Nathan Tipton <nathan.tipton@aggiemail.usu.edu>
2018-10-10 15:09:59 -0600

    changed to c++, finished simulated annealing

--- place.c
+++ place.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 typedef struct Node{
-
-    int x;
-    int y;
+	
+	int x;
+	int y;
+	int num;
+			
 
 } Node;
 
@@ -34,9 +36,14 @@ void main(int argc, char const *argv[]){
     int x=-1;
     int y=-1;
     int numNodes=-1;
-    int arrayIndex=-1;
+    int arrayIndex=0;
     int nodeX=-1;
     int nodeY=-1;
+	int testNumNodes=0;
+	char* test;
+    char* value;
+    const char delim[2]="\n";
+    const char space[2]=" ";
     while((nread = getline(&buffer, &len, fptr))!= -1){
         printf("Retrieved line of length %zu:\n",nread);
         fwrite(buffer,nread,1,stdout);
@@ -66,8 +73,17 @@ void main(int argc, char const *argv[]){
 
         }
         else if(buffer[0]=='v'){
-             for(int i=2;buffer[i] != '\0';i++)
-                if(buffer[i]==' '||buffer[i]=='\r'||buffer[i]=='\n')
+		//sscanf(buffer, "%c %d",test,testNumNodes);
+        value=strtok(buffer,space);
+        printf("value: %s\n",value);
+        test=value;
+
+        value=strtok(NULL,delim);
+        printf("value2: %s",value);
+        testNumNodes=atoi(value);
+            /*
+             for(int i=2;buffer[i] != '\0';i++){
+                 if(buffer[i]==' '||buffer[i]=='\r'||buffer[i]=='\n')
                 {
                     printf("X\n");
                     intBuffindex=0;
@@ -86,72 +102,36 @@ void main(int argc, char const *argv[]){
                     }
                     printf("\n");
                 }
-                Node* array=malloc(numNodes*sizeof(Node));
-        }
+                */
+                Node* array=malloc(testNumNodes*sizeof(Node));
+                
+
+
+           //  }
+                
         
-        else if(buffer[0]=='e'){
-            for(int i=2;buffer[i] != '\0';i++)
-                if(buffer[i]==' '||buffer[i]=='\r'||buffer[i]=='\n')
-                {
-                    printf("X\n");
-                    intBuffindex=0;
-                    if(arrayIndex==-1){
-                        if(nodeX==-1){
-                            nodeX=atoi(intBuff);
-                            intBuff[0]='\0';
-                        }
-                        if(nodeY==-1 && nodeX !=-1){
-                            nodeY=atoi(intBuff);
-                            intBuff[0]='\0';
-                            arrayIndex=0;
-                            array[arrayIndex].x=nodeX;
-                            array[arrayIndex].y=nodeY;
-                            arrayIndex=arrayIndex+1;
-                            nodeY=-1;
-                            nodeX=-1;
-
-                        }
-                        
-
-                    }else if(arrayIndex>0){
-                        if(nodeX==-1){
-                            nodeX=atoi(intBuff);
-                            intBuff[0]='\0';
-                        }
-                        if(nodeY==-1 && nodeX !=-1){
-                            nodeY=atoi(intBuff);
-                            intBuff[0]='\0';
-                            array[arrayIndex].x=nodeX;
-                            array[arrayIndex].y=nodeY;
-                            arrayIndex=arrayIndex+1;
-                            nodeY=-1;
-                            nodeX=-1;
-
-                        }
-                        
-
-                    }
-                }else{
-                    intBuff[intBuffindex]=buffer[i];
-                    intBuffindex=intBuffindex+1;
-                  
-                    for(int z=0;z<(intBuffindex);z++){
-                        printf("%u ",intBuff[z]);
-
-                    }
-                    printf("\n");
-                }
-
 
         }
+        else if(buffer[0]=='e'){
+            value=strtok(buffer,space);
+            value=strtok(NULL,space);
+            nodeX=atoi(value);
+            value=strtok(NULL,delim);
+            array[arrayIndex].num=arrayIndex;
+            array[arrayIndex].x=nodeX;
+            array[arrayIndex].y=nodeY;
+            arrayIndex=arrayIndex+1;
+        }
+
      
     }
     printf("grid is sized %u by %u\n",x,y);
     printf("Number of nodes: %u\n",numNodes);
-    printf("Nodes:\n");
-    for(int i=0;i<numNodes;i++){
-        printf("Node %u is at %u %u\n",i,array[i].x,array[i].y);
-
+	printf("Test number of nodes: %s %d\n",test,testNumNodes);
+  //  printf("Nodes:\n");
+   for(int i=0;i<testNumNodes;i++){
+       //printf("Node %d is at %d %d\n",i,array[i].x,array[i].y);
+       printf("%d\n",i);
     }
     free(buffer);
     fclose(fptr);
---
+++ place.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define INITIAL_TEMPERATURE 10000
+#define COOLING_RATE 0.999
+#define STOP_THRESHOLD 0.0001
+
+class Edge{
+public:
+    int node1;
+    int node2;
+    double length;
+};
+class Node{
+public:
+    int x;
+    int y;
+    int number;
+};
+class Solution{
+    public:
+    vector<Edge> edgeArray;
+    vector<Node> nodeArray;
+    vector<Node> solutionArray;
+    int score;
+
+};
+Solution anneal(Solution initial,int x, int y);
+int evaluate(Solution& s);
+void alter(Solution& s, int x, int y);
+void copy(Solution& current, Solution& next);
+
+int main(int argc, char const *argv[]){
+
+    srand(time(NULL));
+
+    if(argc<3){
+        printf("\n Not enough arguments, please provide input file and output file\n");
+        exit(EXIT_FAILURE);
+
+
+
+    }
+    ofstream fout;
+    ifstream fin;
+
+    fin.open(argv[1]);
+    if(!fin.is_open()){
+
+        cout<<"Error opening input file\n";
+        exit(EXIT_FAILURE);
+    } 
+    fout.open(argv[2]);
+    fout<<"Solution for "<<argv[1]<<"\n";
+    char parameter;
+    int x;
+    int y;
+    int numNodes;
+    int nodeOne;
+    int nodeTwo;
+    int arrayIndex=0;
+   // while(!fin.eof()){
+       fin>>parameter;
+       cout<<parameter<<" ";
+       fin>>x;
+       cout<<x<<" ";
+       fin>>y;
+       cout<<y<<"\n";
+
+       fin>>parameter;
+       cout<<parameter<<" ";
+       fin>>numNodes;
+       cout<<numNodes<<"\n";
+        Solution initial;
+       Edge temp;
+       while(true){
+
+           fin>>parameter;
+           if(fin.eof())break;
+           cout<<parameter<<" ";
+           fin>>nodeOne;
+           cout<<nodeOne<<" ";
+           fin>>nodeTwo;
+           cout<<nodeTwo<<"\n";
+           temp.node1=nodeOne;
+           temp.node2=nodeTwo;
+           initial.edgeArray.push_back(temp);
+
+
+            
+       }
+
+        for(int i=0; i<initial.edgeArray.size();i++){
+            cout<<"Edge from "<<initial.edgeArray[i].node1<<" "<<initial.edgeArray[i].node2<<"\n";
+
+
+        }
+        for(int i=0; i<numNodes;i++){
+            Node temp;
+            temp.x=rand()%x;
+            temp.y=rand()%y;
+            temp.number=i;
+            initial.nodeArray.push_back(temp);
+
+        }
+        for(int i=0; i<initial.nodeArray.size();i++){
+
+            cout<<"Node "<<initial.nodeArray[i].number<<" placed at ("<<initial.nodeArray[i].x<<","<<initial.nodeArray[i].y<<")\n";
+        }
+   // }
+   cout<<"Grid is size "<<x<<" by "<<y<<"\n";
+    Solution finalSolution;
+    finalSolution=anneal(initial,x,y);
+    fout<<"Final score: "<<finalSolution.score<<"\n";
+  
+     for(int i=0; i<finalSolution.nodeArray.size();i++){
+
+            fout<<"Node "<<finalSolution.nodeArray[i].number<<" placed at ("<<finalSolution.nodeArray[i].x<<","<<finalSolution.nodeArray[i].y<<")\n";
+        }
+    for(int i=0; i<finalSolution.edgeArray.size();i++){
+        fout<<"Edge from "<<finalSolution.edgeArray[i].node1<<" to "<<finalSolution.edgeArray[i].node2<<" has length "<<finalSolution.edgeArray[i].length<<"\n";
+    }
+    fout.close();
+    fin.close();
+    return 0;
+}
+
+Solution anneal(Solution initial,int x, int y){
+
+    Solution finalSolution;
+    Solution current;
+    Solution next;
+    int deltaE;
+    float p,r;
+    float temperature;
+    temperature=INITIAL_TEMPERATURE;
+    initial.score=evaluate(initial);
+    for(int i=0;i<initial.edgeArray.size();i++){
+        cout<<"edge length "<<initial.edgeArray[i].length<<"\n";
+    }
+    cout<<"Initial Score: "<<initial.score<<"\n";
+    copy(initial,current);
+    cout<<"Current Score: "<<current.score<<"\n";
+    while((temperature > STOP_THRESHOLD)){
+
+        copy(current,next);
+        alter(next,x,y);
+        next.score=evaluate(next);
+       // cout<<"new score: "<<next.score<<"\n";
+        if(next.score<current.score){
+            copy(next,current);
+        }else{
+            deltaE=abs(current.score-next.score);
+            p= exp(-((float)deltaE)/temperature);
+            r = (float) rand() / (float) RAND_MAX;
+            if(r<p){
+                copy(next,current);
+            }
+        }
+        
+        temperature=temperature*COOLING_RATE;
+       // cout<<temperature<<"\n";
+    }
+
+    copy(current,finalSolution);
+  
+
+
+    return finalSolution;
+}
+void copy(Solution& current, Solution& next){
+next.edgeArray=current.edgeArray;
+next.nodeArray=current.nodeArray;
+next.score=current.score;
+
+
+
+
+}
+int evaluate(Solution& s){
+    int currentScore=0;
+    double distance=0;
+    //calculate distance between nodes based on edges
+    for(int i=0;i<s.edgeArray.size();i++){
+        distance=abs(s.nodeArray[s.edgeArray[i].node1].x-s.nodeArray[s.edgeArray[i].node2].x)+abs(s.nodeArray[s.edgeArray[i].node1].y-s.nodeArray[s.edgeArray[i].node2].y);       
+       // cout<<"distance: "<<distance<<"\n";
+        s.edgeArray[i].length=distance;
+        currentScore=currentScore+distance;
+    }
+    //check for overlapping nodes
+
+    for(int i=0;i<s.nodeArray.size();i++){
+        for(int j=i+1;j<s.nodeArray.size();j++){
+
+            if(s.nodeArray[i].x==s.nodeArray[j].x){
+                if(s.nodeArray[i].y==s.nodeArray[j].y){
+                    if(i != j){
+                        currentScore=currentScore+1000;
+                       // cout<<"Nodes "<<i<<" and "<<j<<" overlap\n";
+                    }
+                }
+
+
+            }
+        }
+
+    }
+
+
+
+    return currentScore;
+
+}
+
+void alter(Solution& s, int x, int y){
+    
+    int changeIndex;
+    changeIndex=rand()%s.nodeArray.size();
+    s.nodeArray[changeIndex].x=rand()%x;
+    s.nodeArray[changeIndex].y=rand()%y;
+
+
+
+
+
+}
